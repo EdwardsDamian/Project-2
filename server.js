128f1a5b1cfec1b8e6277f976be965e2c8a9aea9
EdwardsDamian <damianedwards@msn.com>
2019-04-25 12:49:03 -0400

    2nd commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const methodOverride = require('method-override')
+
+
+//Include middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
+app.use(express.static(__dirname + '/public'))
+
+app.set('view engine', 'hbs')
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`)
+})
