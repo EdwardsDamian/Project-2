f616dbd533b8c9c1031fd1223c65c253e2935a04
EdwardsDamian <damianedwards@msn.com>
2019-04-26 11:53:28 -0400

    user data has been seeded

--- DB/seeds.js
+++ DB/seeds.js
@@ -1,40 +1,36 @@
 const userApi = require('../api/userApi')
 
-// let newUser = [
-//     {
-//         name: "Analise",
-//         age: 9,
-//         grade: "3rd"
-//     },
+let newUser = [
+    {
+        name: "Analise",
+        age: 9,
+        grade: "3rd"
+    },
 
-//     {
-//         name: "Bill",
-//         age: 8,
-//         grade: "3rd"
-//     },    
+    {
+        name: "Bill",
+        age: 8,
+        grade: "3rd"
+    },    
 
-//     {
-//         name: "Chloe",
-//         age: 9,
-//         grade: "3rd"
-//     },
+    {
+        name: "Chloe",
+        age: 9,
+        grade: "3rd"
+    },
 
-//     {
-//         name: "Dominic",
-//         age: 8,
-//         grade: "3rd"
-//     }
-// ]
+    {
+        name: "Dominic",
+        age: 8,
+        grade: "3rd"
+    }
+]
 
-userApi.createUser({
-            name: "Analise",
-            age: 9,
-            grade: "3rd"
-        }).then(newUser => {
+userApi.createUser(newUser).then(newUser => {
     console.log(newUser);
 });
 
-userApi.getUserById("5cc317df7c3d328a98f572d5")
-    .then(userId => {
-        console.log(userId);
-    });
+// userApi.getUserById("5cc317df7c3d328a98f572d5")
+//     .then(userId => {
+//         console.log(userId);
+//     });
