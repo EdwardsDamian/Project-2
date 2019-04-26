90bd66c3b5e043d8e85f76ea09c90de9e220a645
EdwardsDamian <damianedwards@msn.com>
2019-04-26 14:58:00 -0400

    more views connections

--- DB/connection.js
+++ DB/connection.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 
 const connectionString = "mongodb://localhost/Project-2";
 
-//newUrlParser to disable deprecation warning
-mongoose.connect(connectionString, {useNewUrlParser: true})
-    .then(() => {
-        console.log("connected to mongo at: " + connectionString);
-    });
+// Connect to database - enables to use the mongo db created by heroku once you deploy
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+}   else {
+    mongoose.connect(connectionString, {useNewUrlParser: true})
+}
+mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
+
 module.exports = mongoose;
